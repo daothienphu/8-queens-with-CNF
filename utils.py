2eb03bd126a7c43c8e5eea6f676fb9c0fb888286
peternguyen39 <54886011+peternguyen39@users.noreply.github.com>
2021-12-25 23:44:58 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,14 +3,26 @@ from typing import List
 from pysat.formula import CNF
 from pysat.solvers import Lingeling
 
-def generate_cnf_(size, verbose=False):
-    restrictions = []
+def generate_cnf_level_2(size, verbose=False):
+    restrictions = [[i for i in range(1,size*size+1)]]
     for i in range(size*size):
-        current_restriction = [(i+1)*-1]
         for j in range(size*size):
             if (i!=j)and((j % size == i %size) or (j // size == i // size) or (abs(i//size-j//size)==abs(i%size-j%size))):
-                current_restriction.append((j+1)*-1)
-        restrictions.append(current_restriction)
+                restrictions.append([-1*(i+1),(j+1)*-1])
+    if verbose:
+        for i in restrictions:
+            print(i)
+    return restrictions
+
+def generate_cnf_level_1(size, verbose=False):
+    restrictions = []
+    for i in range(size):
+        current=[i for i in range(i*8+1,i*8+9)]
+        restrictions.append(current)
+    for i in range(size*size):
+        for j in range(size*size):
+            if (i!=j)and((j % size == i %size) or (j // size == i // size) or (abs(i//size-j//size)==abs(i%size-j%size))):
+                restrictions.append([-1*(i+1),(j+1)*-1])
     if verbose:
         for i in restrictions:
             print(i)
