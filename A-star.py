0444edc82ea653dd83c156e54cf18efaa1c3ec89
CuzzImBatman <lgbao19@apcs.vn>
2021-12-26 22:58:31 +0700

    Update A-star.py

--- A-star.py
+++ A-star.py
@@ -1,6 +1,5 @@
 import random
 import numpy as np
-import tensorflow as tf
 import math
 with open("input.txt", "r") as file: 
     m = int(file.readline())
@@ -83,9 +82,12 @@ for pos in queens_pos:
 
 avail_row_idx = [i  for i in range(len(cols)) if cols[i] == 0]
 print(diagonal_main)
-decoded = tf.argmax(table, axis=1)
+#decoded = tf.argmax(table, axis=1)
 
-decoded = np.array(decoded)
+decoded= [0]*8
+for pos in queens_pos:
+        decoded[pos[0]]= pos[1 ]+1
+#decoded = np.array(decoded)
 SEQS= list(decoded)
 frontier_priority_queue = [{'unplaced_queens':8, 'pairs':28+8, 'seqs':SEQS}] # The initial state is 8 zeros, which means that there are no queens on the chessboard; g(n) = the number of queens that have not been placed well, h(n) = the logarithm of queens attacking each other, and h(n)=28, g(n)=8
 solution = []
