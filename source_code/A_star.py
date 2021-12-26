fd50ea33c73e6d9acb8eb04bc8fdd8124a1c918d
CuzzImBatman <lgbao19@apcs.vn>
2021-12-27 00:21:41 +0700

    Update A_star.py

--- source_code/A_star.py
+++ source_code/A_star.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 import math
 # with open("input.txt", "r") as file: 
 #     m = int(file.readline())
@@ -61,16 +61,16 @@ class A_star:
         """
         Displays the chessboard corresponding to the sequence
         """
-        board = np.array([0] * 81)  # Create a one-dimensional array with 81 zeros
-        board = board.reshape(9, 9)  # Change to a 9 * 9 two-dimensional array. For the convenience of later use, only the 8 * 8 parts of the last eight rows and columns are used as a blank chessboard
+        board = np.array([0] * 81)  
+        board = board.reshape(9, 9)  
 
         for i in range(1, 9):
-            board[seqs[i - 1]][i] = 1  # According to the sequence, from the first column to the last column, put a queen in the corresponding position to generate the chessboard corresponding to the current sequence
+            board[seqs[i - 1]][i] = 1 
         print('The corresponding chessboard is as follows:')
         for i in board[1:]:
             for j in i[1:]:
-                print(j, ' ', end="")  # With end, print doesn't wrap
-            print()  # After outputting one line, wrap it. This cannot be print('\n'), otherwise it will be replaced by two lines
+                print(j, ' ', end="") 
+            print() 
         print('The number of queens to attack is' + str(self.attacked_queens_pairs(seqs)) + "  f(n)= " +str(self.attacked_queens_pairs(seqs)+ self.next_row_invalids(seqs)))
     
     def solve(self):
@@ -87,17 +87,17 @@ class A_star:
 
         avail_row_idx = [i  for i in range(len(cols)) if cols[i] == 0]
         print(diagonal_main)
-        decoded = tf.argmax(table, axis=1)
-
-        decoded = np.array(decoded)
+        decoded= [0]*8
+        for pos in self.queens_pos:
+             decoded[pos[0]]= pos[1 ]+1
         SEQS= list(decoded)
-        frontier_priority_queue = [{'unplaced_queens':8, 'pairs':28+8, 'seqs':SEQS}] # The initial state is 8 zeros, which means that there are no queens on the chessboard; g(n) = the number of queens that have not been placed well, h(n) = the logarithm of queens attacking each other, and h(n)=28, g(n)=8
+        frontier_priority_queue = [{'unplaced_queens':8, 'pairs':28+8, 'seqs':SEQS}] 
         solution = []
-        flag = 0 # The representative has not found a solution
+        flag = 0 
 
-        while frontier_priority_queue: # If the frontier is not empty, the loop will continue. If the solution is found successfully, the loop will output the solution. If the frontier is empty, the loop will fail
-            first = frontier_priority_queue.pop(0)  # Since frontier s are sorted every time, the first sequence is extended
-            if first['pairs'] == 8 and first['unplaced_queens'] == 0: # Do the goal test before extending the node: if the sequence h(n)=g(n)=0, then the sequence is the solution sequence
+        while frontier_priority_queue:
+            first = frontier_priority_queue.pop(0) 
+            if first['pairs'] == 8 and first['unplaced_queens'] == 0:
                 solution = first['seqs']
                 flag = 1  # success
                 break
@@ -108,20 +108,19 @@ class A_star:
             LIST = list(filter(lambda x: x == 0, seqs))
             self.display_board(seqs)
             if(len(LIST)>0):
-                for j in range(len(nums)): # In the first position of 0 in the sequence, that is, the leftmost column without queen, select a row to place queen
+                for j in range(len(nums)): 
                     pos = seqs.index(0)
                     temp_seqs = list(seqs)
-                    temp = random.choice(nums)  # Select a random row in the column to place the queen
+                    temp = random.choice(nums)  
                     nums.remove(temp)
                     if ((diagonal_main[pos + temp ] ==0) & (diagonal_sub [temp- pos +7]==0) ):
-                        temp_seqs[pos] = temp +1 # Place the queen on line temp of the column
-                        # Remove generated values from nums
+                        temp_seqs[pos] = temp +1
                         print("     "+ str(temp_seqs))
                         #print("     "+ str(nums))
                         #print("     "+ str(temp)+ " "+str(pos))
                         
                         frontier_priority_queue.append({'unplaced_queens':temp_seqs.count(0), 'pairs':8*self.attacked_queens_pairs(temp_seqs) + self.next_row_invalids(temp_seqs),'seqs':temp_seqs})
-                frontier_priority_queue = sorted(frontier_priority_queue, key=lambda x:(x['pairs'], x['unplaced_queens'])) # First, the sequences are sorted in the order of h(n) from small to large; if h(n) is the same, the sequences are sorted in the order of g(n) from small to large
+                frontier_priority_queue = sorted(frontier_priority_queue, key=lambda x:(x['pairs'], x['unplaced_queens']))
 
 
         if solution:
@@ -133,4 +132,4 @@ class A_star:
             return solution2[len(self.queens_pos):]
         else:
             return []
-            #print('Algorithm failed, no solution found')
+            
